569f3411fcfce90f7968d502d9b518d3ed97c872
Paul Serraino <pserraino99@gmail.com>
2015-07-25 18:40:54 -0500

    working commit

--- example/main.js
+++ example/main.js
@@ -1,25 +1,15 @@
-# react-geojson-cluster
-
-```
-npm install react-geojson-cluster
-```
-
-## example
-```js
 import React from 'react';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
-import { GeoJsonCluster } from'react-geojson-cluster';
+import { GeoJsonCluster } from'../';
+
+import geojson from './example-geojson.js';
 
 const map = <Map id="map" center={[30.25, -97.75]} zoom={13}>
   <TileLayer
     url='https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png'
     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     id='paulserraino.n0dn3pbe' />
-  <GeoJsonCluster data={your_geojson} />
+  <GeoJsonCluster data={geojson} />
 </Map>;
 
 React.render(map, document.getElementById('map-container'));
-```
-
-## License
-MIT
