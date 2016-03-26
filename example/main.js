99eff7bc2f4a430363527aaa13da4e3b9cf94f09
Paul Serraino <pserraino99@gmail.com>
2016-03-26 17:33:13 -0500

    fix example

--- example/main.js
+++ example/main.js
@@ -1,43 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 import { GeoJsonCluster } from'../';
-import toGeoJSON from './to-geojson';
 import request from 'superagent';
 
-class App extends React.Component {
+class App extends Component {
 
-  constructor (props) {
-    super(props);
-    this.state = {
-      buses: null
-    };
-  }
+  state = {
+    buses: null
+  };
 
   componentWillMount() {
     request
-      .get('https://data.texas.gov/resource/9e7h-gz56.json')
+      .get('data/vehicleposition.json')
       .end(function(error, response){
         if (error) return console.error(error);
 
         this.setState({
-          buses: toGeoJSON(response.body)
-        })
+          buses: response.body
+        });
 
       }.bind(this));
   }
 
   render () {
+    const { buses } = this.state;
+
+    if (!buses) {
+        return (<div>loading...</div>);
+    }
+
     return (
       <Map id="map" center={[30.25, -97.75]} zoom={13}>
         <TileLayer
           url='https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png'
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           id='paulserraino.n0dn3pbe' />
-        <GeoJsonCluster data={this.state.buses} />
+        <GeoJsonCluster data={buses} />
       </Map>
     );
   }
 
 }
 
-React.render(<App />, document.getElementById('map-container'));
+render(<App />, document.getElementById('map-container'));
